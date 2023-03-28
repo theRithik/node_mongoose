238ac4c09347afba9f93fd694ab3b9784d6e2999
rithik <115656594+theRithik@users.noreply.github.com>
2023-03-28 23:31:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cityDetails = require('./model/citId')
+
+mongoose.connect('mongodb://127.0.0.1:27017/cityDetails')
+
+app.get('/',(req, res)=>{
+    res.send('this from the express- mongoose')
+
+})
+
+app.post('/addDetails',(req,response)=>{
+
+    cityDetails.create(req.body,(err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            response.send('data added successfully')
+        }
+    })
+})
+
+//read end point
+app.get('/getDetails',(req,response)=>{
+
+    cityDetails.find((err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            response.send(result)
+        }
+    })
+})
+
+//update end point
+app.put('/updateDetails',(req,response)=>{
+
+    cityDetails.findOneAndUpdate({_id:req.body._id},{$set:{city:req.body.city}},(err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            response.send('data added successfully')
+        }
+    })
+})
+
+
+app.listen(4004, ()=>{
+    console.log('running in port number 4004')
+})
